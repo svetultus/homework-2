f061c003137d5125bbe5c703992f8b78484b459a
svetultus <svetultus@gmail.com>
2019-08-01 13:26:10 +0300

    homework-2

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import Message from '../Message';
+
+class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      messageInput: '',
+      count: 0
+    };
+
+    this.changeInputMessage = this.changeInputMessage.bind(this);
+    this.sendMessageOnEnter = this.sendMessageOnEnter.bind(this);
+  }
+
+  changeInputMessage(e) {
+    this.setState({ messageInput: e.target.value });
+  }
+
+  sendMessageOnEnter(e) {
+    if (e.key === 'Enter') {
+      this.setState({
+        messages: [
+          ...this.state.messages,
+          {
+            text: this.state.messageInput
+            //id: this.state.count + 1
+          }
+        ],
+        count: this.state.count + 1,
+        messageInput: ''
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="chat">
+        <ul>
+          {this.state.messages.map((item, index) => {
+            return (
+              <Message
+                //key={item.id}
+                key={index}
+                text={item.text}
+              />
+            );
+          })}
+        </ul>
+        <input
+          className="input-message"
+          value={this.state.messageInput}
+          onChange={this.changeInputMessage}
+          onKeyPress={this.sendMessageOnEnter}
+        />
+      </div>
+    );
+  }
+}
+
+export default Chat;
