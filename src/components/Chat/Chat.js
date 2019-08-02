fc33fb9637cdfc78107b52aac7ae8d3a46cd51ee
svetultus <svetultus@gmail.com>
2019-08-02 11:19:08 +0300

    fixes

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -20,37 +20,28 @@ class Chat extends React.Component {
 
   sendMessageOnEnter(e) {
     if (e.key === 'Enter') {
-      this.setState({
-        messages: [
-          ...this.state.messages,
-          {
-            text: this.state.messageInput
-            //id: this.state.count + 1
-          }
-        ],
-        count: this.state.count + 1,
-        messageInput: ''
+      this.setState(prevState => {
+        return {
+          messages: [...prevState.messages, { text: prevState.messageInput }],
+          count: prevState.count + 1,
+          messageInput: ''
+        };
       });
     }
   }
 
   render() {
+    const { messages, messageInput } = this.state;
     return (
       <div className="chat">
         <ul>
-          {this.state.messages.map((item, index) => {
-            return (
-              <Message
-                //key={item.id}
-                key={index}
-                text={item.text}
-              />
-            );
+          {messages.map((item, index) => {
+            return <Message key={index} text={item.text} />;
           })}
         </ul>
         <input
           className="input-message"
-          value={this.state.messageInput}
+          value={messageInput}
           onChange={this.changeInputMessage}
           onKeyPress={this.sendMessageOnEnter}
         />
